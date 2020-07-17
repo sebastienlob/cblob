49c2230284417751297a345a854d9b62a07cc594
sebastienlob <lobseb@gmail.com>
2020-07-17 12:04:08 -0400

    enumeration

--- scripts/essai_JS.js
+++ scripts/essai_JS.js
@@ -1,1 +1,13 @@
-document.writeln('hello wolrd!');
+document.writeln('hello world!');
+
+var i;
+var properties = [
+    'first-name',
+    'middle-name',
+    'last-name',
+    'profession',
+];
+ for (i = 0; i < properties.length; i += 1){
+     document.writeln(properties[i] + ':' + 
+            another_stooge[properties[i]]);
+ }
