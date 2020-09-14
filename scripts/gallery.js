3709855fdd32cf17ebec356dce04232560192f12
sebastienlob <lobseb@gmail.com>
2020-09-14 18:12:43 -0400

    change size

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,7 +1,7 @@
 const pageImage = document.querySelector('.pageimage');
 
 for (let i = 1; i<=14; i++){
-let sizeArray = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34];
+let sizeArray = [12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34];
 var randomItem = sizeArray[Math.floor(Math.random()*sizeArray.length)];
 const newImage = document.createElement('img');
 newImage.setAttribute('src', 'image/travels/thailand/IMG'+ i +'.JPG');
