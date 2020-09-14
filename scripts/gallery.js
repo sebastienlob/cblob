f6cdbef0b685c1271cb9269b45c6ee1dc19cc2d0
sebastienlob <lobseb@gmail.com>
2020-09-14 17:34:50 -0400

    new gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,17 @@
 const pageImage = document.querySelector('.pageimage');
+/*const styleTravel = document.createElement('style')
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)*/
+
+
+
 for (let i = 1; i<=14; i++){
+let sizeArray = [20, 25, 30, 35, 40, 45];
+var randomItem = sizeArray[Math.floor(Math.random()*sizeArray.length)];
 const newImage = document.createElement('img');
 newImage.setAttribute('src', 'image/travels/thailand/IMG'+ i +'.JPG');
+newImage.setAttribute('width', randomItem + '%');
+newImage.setAttribute('height', randomItem + '%');
 pageImage.appendChild(newImage);
 }
