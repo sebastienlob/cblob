aad8080951925d251f66d918bcde8a64a39628b4
sebastienlob <lobseb@gmail.com>
2020-09-15 08:41:48 -0400

    change name

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,7 +4,7 @@ for (let i = 1; i<=14; i++){
 let sizeArray = [12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34];
 var randomItem = sizeArray[Math.floor(Math.random()*sizeArray.length)];
 const newImage = document.createElement('img');
-newImage.setAttribute('src', 'image/travels/thailand/IMG'+ i +'.JPG');
+newImage.setAttribute('src', 'image/travels/thailand/thai'+ i +'.JPG');
 newImage.setAttribute('width', randomItem + '%');
 newImage.setAttribute('height', randomItem + '%');
 pageImage.appendChild(newImage);
