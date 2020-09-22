10a3204d2153f4f732299f8552d91a7fdf4aeb8d
sebastienlob <lobseb@gmail.com>
2020-09-22 13:24:37 -0400

    incremental button

--- scripts/gallerycb.js
+++ scripts/gallerycb.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 const divZoomed = document.querySelector('.zoomed');
 const zoomedImage = document.querySelector('.CAB');
 const X = document.querySelector('.X');
+const rArrow = document.querySelector('.rarrow')
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 divZoomed.style.visibility = 'hidden';
@@ -19,14 +20,28 @@ function zoomIn (e) {
     zoomedImage.setAttribute('width', zoomW);
     zoomedImage.setAttribute('height', zoomH);
     X.setAttribute('src', 'image/travels/cabin/X2.png')
+    rArrow.setAttribute('src', 'image/travels/cabin/rarrow.png')
     divZoomed.appendChild(zoomedImage);
     divZoomed.appendChild(X);
+    divZoomed.appendChild(rArrow);
     X.addEventListener('click', quitZoom);
+    rArrow.addEventListener('click', scrollRight);
     }
 
 /* fonction pour fermer l'image zoomée */
 function quitZoom (){
     divZoomed.style.visibility = 'hidden';
+}
+
+/* fonction pour faire deffiler les images vers la droite */
+
+function scrollRight () {
+    let zoomedSrc = zoomedImage.src;
+    let j = zoomedSrc[28];
+    console.log(zoomedSrc);
+    console.log(j);
+    /*zoomedImage.setAttribute('src', 'image/travels/cabin/ocab'+ j +'.JPG');
+    divZoomed.appendChild(zoomedImage);*/
 }
 
 /* pour afficher l'ensemble des images avec une taille aléatoire dans la page */
@@ -39,7 +54,6 @@ for (let i = 1; i<=12; i++){
     newImage.setAttribute('class', 'image');
     newImage.setAttribute('width', randomItem + '%');
     newImage.setAttribute('height', randomItem + '%');
-    /*newImage.setAttribute('id', 'IMG' + i);*/
     pageImage.appendChild(newImage);
     newImage.addEventListener('click', zoomIn);
 }
